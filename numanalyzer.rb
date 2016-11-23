b50f839aea68c5b91a95e87c526389d51c1b1b39
Joshua Philips <jmichaelphilips@gmail.com>
2016-11-22 22:56:07 -0600

    added a different analyzing function working with numbers

---
+++ numanalyzer.rb
@@ -0,0 +1,5 @@
+puts "Give me a number"
+first_number = gets.chomp
+puts "And another"
+second_number = gets.chomp
+puts "#{first_number} multiplied by #{second_number} equals #{second_number.to_i * first_number.to_i}"
